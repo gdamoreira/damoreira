b4026346b69ba6c21f0e2d942b8e717769621e8c
Guilherme D'Amoreira <guilherme@damoreira.com.br>
2015-06-02 20:07:37 +0000

    Mudanças significativas

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){    
-    $('.myCarousel').carousel({
+    $(".myCarousel").carousel({
         interval: 2000
     })
-    $('#printBtn').click(function(){
+    $("#printBtn").click(function(){
         print();
     });
+    var hoje = new Date();
+    var idade = (hoje.getFullYear() - 1991) - 1;
+    if (hoje.getMonth() > 7 || (hoje.getMonth() == 7 && hoje.getDate() >= 23)) {
+        idade++;
+    }
+    $("#idade").html(idade);
 });
 
